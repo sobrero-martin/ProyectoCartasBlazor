ef10ca89b1dbb8c5383c7f2aede6d3993a0d3106
sobrero-martin <sobrero.martin26@gmail.com>
2025-08-20 19:46:43 -0300

    Clases de tablas agregadas

---
+++ ProyectoCartas.BD/Datos/Entidades/Sobres.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCartas.BD.Datos.Entidades
+{
+    internal class Sobres
+    {
+    }
+}
---
+++ ProyectoCartas.Shared/ENUM/EstadoRegistro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCartas.Shared.ENUM
+{
+    public enum EstadoRegistro
+    {
+        Activo = 1,
+        Inactivo = 2,
+        Borrado = 3
+    }
+}
